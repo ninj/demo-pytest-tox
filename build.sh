fbbb2ea1f50266b24d690a0140c2f5e398454ea1
Steve Wong <swong@metahq.com>
2021-11-06 22:15:04 +0000

    ! help output via cat instead of comments for more flexibility

--- build.sh
+++ build.sh
@@ -1,46 +1,26 @@
 #!/usr/bin/env bash
 
-# SUMMARY
-#   Stores project build conventions
-#
-# USAGE
-#   build.sh [-?|-h|--help] [goal...]
-#
-# PRE-REQUISITES
-#   - pipx installed outside of venv for project-local installations of tools (pip install --user pipx)
-#   - venv active, using target python version 3.7 (python3 -m venv venv && source venv/bin/activate)
-#   - other python versions available for tox to use (see .python-version file for pyenv)
-#
-# VERSION MANAGEMENT OF TOOLS FOR LOCAL DEVELOPMENT
-#   - build.sh itself only relies on toxw
-#   => tox version is managed in .tox/.wrapper/toxw.config
-#   - bootstrap goal uses tox to create developer virtualenv
-#   => tool versions that are installed into developer virtual environment are managed in tox.ini
-#
-# EXAMPLES
-#
-# build project:
-#   ./build.sh
-#   or
-#   ./build.sh assemble
-#
-# bootstrap tooling for local development:
-#   ./build.sh bootstrap
-#
-# export vars to run project-local tools, e.g. tox:
-#   eval "$(./build.sh init)"
-#   tox ...
-#
-# generate requirements files using pip-compile:
-#   ./build.sh pip-compile
-#
-# update venv from dev-requirements.txt:
-#   ./build.sh pip-sync
-#
 set -ueo pipefail
 
-declare -r SCRIPT_ORIG="$0"
 declare -r SCRIPT_NAME="${0##*/}"
+
+help() {
+  cat <<__HELP__
+SUMMARY
+  builds project
+
+USAGE
+  $SCRIPT_NAME [-?|-h|--help] [goal...]
+
+EXAMPLES
+
+build project:
+  $SCRIPT_NAME
+  or
+  $SCRIPT_NAME assemble
+__HELP__
+}
+
 
 is_virtualenv() {
   [[ -n "${VIRTUAL_ENV:-}" ]]
@@ -61,20 +41,6 @@ __DOCTOR__
 goal_assemble() {
   goal_bootstrap
   invoke assemble
-}
-
-help() {
-  in_block=
-  while IFS= read -r line; do
-    if [[ "$line" == "#!"* ]]; then
-      continue
-    elif [[ "$line" == "#"* ]]; then
-      in_block=1
-    elif [[ -n "$in_block" ]]; then
-      return 0
-    fi
-    [[ -n "$in_block" ]] && printf "%s\n" "$line"
-  done < "$SCRIPT_ORIG"
 }
 
 goal_help() {
